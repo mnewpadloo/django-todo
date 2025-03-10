e8fdb963924ec0c40a12c175dc8ab8e6f0a837b3
mnewpadloo <mishhhin@gmail.com>
2025-03-11 01:48:55 +0500

    Исправление ошибок

--- tasks/models.py
+++ tasks/models.py
@@ -6,17 +6,22 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+    class Meta:
+        verbose_name_plural = "Categories"
+
 class Priority(models.Model):
     level = models.CharField(max_length=50)
 
     def __str__(self):
         return self.level
 
+    class Meta:
+        verbose_name_plural = "Priorities"
+
 class Task(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField(blank=True, max_length=300)
     completed = models.BooleanField(default=False)
-    # created_at = models.DateTimeField(auto_now_add=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True, blank=True)
     priority = models.ForeignKey(Priority, on_delete=models.SET_NULL, null=True, blank=True)
 
